174b0ce8f21c0623c5453d4249c16e0a9215e117
LegendOfOm <omkhalil2007@gmail.com>
2026-07-29 16:33:37 -0700

    just saving changes

--- GameLogic.cpp
+++ GameLogic.cpp
@@ -1,6 +1,7 @@
 #include "Player.hpp"
 #include "Bridge.hpp"
 #include "GameLogic.hpp"
+#include "Card.hpp"
 #include <iostream>
 
 GameLogic::GameLogic() : bridge(), player1(1, bridge.getPlayer1Castle()), player2(2, bridge.getPlayer2Castle()), turnNumber(0), gameOver(false) {
@@ -45,6 +46,17 @@ void GameLogic::drawingPhase(Player& player) {
 
 void GameLogic::placingPhase(Player& player) {
     std::cout << "Placing phase for player " << player.getPlayerId() << std::endl;
+    for (int i = 0; i < player.getHandSize(); i++) {
+        if (player.getHandSize() == 0) {
+            std::cout << "No cards in hand" << std::endl;
+            break;
+        }
+        if (player.getCardInHand(i) == nullptr) {
+            std::cout << "No card in hand" << std::endl;
+        } else {
+            std::cout << "Card in hand: " << player.getCardInHand(i)->getName() << std::endl;
+        }
+    }
 }
 
 void GameLogic::movingPhase(Player& player) {
@@ -52,5 +64,11 @@ void GameLogic::movingPhase(Player& player) {
 }
 
 void GameLogic::checkWinCondition() {
-    std::cout << "Checking win condition..." << std::endl;
+    if (player1.getCurrentNode() == player2.getCastleNode()) {
+        std::cout << "Player 1 has won the game!" << std::endl;
+        gameOver = true;
+    } else if (player2.getCurrentNode() == player1.getCastleNode()) {
+        std::cout << "Player 2 has won the game!" << std::endl;
+        gameOver = true;
+    }
 }
